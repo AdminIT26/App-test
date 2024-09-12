04745bacfc1c4446419d03f49c416aef34d1cd00
Lee Wei Tat <WeiTat.Lee@argentrasg.local>
2024-09-12 16:06:02 +0800

    12Sep2024_16_05

--- JavaSourceCode_12Sep2024_15_55.java
+++ JavaSourceCode_12Sep2024_15_55.java
@@ -2,7 +2,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Print a message to the console
         System.out.println("Hello, World ");
-	System.out.printIn("Forcepoint DLP");
-
     }
 }
