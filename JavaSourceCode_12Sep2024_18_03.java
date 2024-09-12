24cebbd180fa8ff4d60db83f19595423ef848299
Lee Wei Tat <WeiTat.Lee@argentrasg.local>
2024-09-12 20:42:32 +0800

    12Sep2024_20_42

--- JavaSourceCode_12Sep2024_18_03.java
+++ JavaSourceCode_12Sep2024_18_03.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Print a message to the console
-        System.out.println("Hello, World");
- 	System.out.println("Hello, World123");
+        System.out.println("Hello, World !! ");
     }
 }
